029c8e06e2eb3fde4e86b43ceba4907d2092e28d
MuhammadSaad991 <i191891@nu.edu.pk>
2023-05-07 20:36:37 +0500

    DVC Pipeline Successfully Execute

--- src/get_data.py
+++ src/get_data.py
@@ -2,10 +2,20 @@ import zipfile
 import subprocess
 import pandas as pd
 import os
+import dvc.api
 
-if os.path.isfile("data/creditcard.csv"):
-    n = 1
+
+# DVC storage remote and file path
+remote = 'storage'
+file_path = 'data/creditcard.csv'
+
+if os.path.isfile(file_path):
+    df = pd.read_csv(file_path)
 else:
+    with dvc.api.open(path=file_path, mode='r', remote=remote, repo='.') as fd:
+        df = pd.read_csv(fd)
+        
+if df.empty:
     subprocess.run(['kaggle', 'datasets', 'download', '-d', 'mlg-ulb/creditcardfraud'])
     
     with zipfile.ZipFile('./creditcardfraud.zip', 'r') as zip_ref:
@@ -39,7 +49,10 @@ else:
 # dvc dag
 
 
-# git add .gitignore
+# git add dvc.lock 'data\.gitignore' .gitignore
 # git commit -m "Add .gitignore file to ignore data folder"
 
 # git push origin main
+
+
+
