9723e4c78c34cecf120d84fb97cb41131033e7c5
MuhammadSaad991 <i191891@nu.edu.pk>
2023-05-07 16:30:49 +0500

    DVC Setup

---
+++ src/preprocessing_data.py
@@ -0,0 +1,26 @@
+import numpy as np 
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, RobustScaler
+
+# RobustScaler 
+std_scaler = StandardScaler()
+rob_scaler = RobustScaler()
+df = pd.read_csv('./creditcard.csv')
+
+# The classes are heavily skewed we need to solve this issue later.
+print('No Frauds', round(df['Class'].value_counts()[0]/len(df) * 100,2), '% of the dataset')
+print('Frauds', round(df['Class'].value_counts()[1]/len(df) * 100,2), '% of the dataset')
+
+df['scaled_amount'] = rob_scaler.fit_transform(df['Amount'].values.reshape(-1,1))
+df['scaled_time'] = rob_scaler.fit_transform(df['Time'].values.reshape(-1,1))
+
+df.drop(['Time','Amount'], axis=1, inplace=True)
+
+scaled_amount = df['scaled_amount']
+scaled_time = df['scaled_time']
+
+df.drop(['scaled_amount', 'scaled_time'], axis=1, inplace=True)
+df.insert(0, 'scaled_amount', scaled_amount)
+df.insert(1, 'scaled_time', scaled_time)
+
+df.to_csv("data_preprocessed.csv")
---
+++ src/train.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from catboost import CatBoostClassifier
+from sklearn import svm
+from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score, accuracy_score, classification_report
+from sklearn.model_selection import cross_val_score
+import json
+import pickle
+RFC_METRIC = 'gini'  #metric used for RandomForrestClassifier
+NUM_ESTIMATORS = 100 #number of estimators used for RandomForrestClassifier
+NO_JOBS = 4 #number of parallel jobs used for RandomForrestClassifier
+#TRAIN/VALIDATION/TEST SPLIT
+#VALIDATION
+VALID_SIZE = 0.20 # simple validation using train_test_split
+TEST_SIZE = 0.20 # test size using_train_test_split
+#CROSS-VALIDATION
+NUMBER_KFOLDS = 5 #number of KFolds for cross-validation
+RANDOM_STATE = 42
+MAX_ROUNDS = 1000 #lgb iterations
+EARLY_STOP = 50 #lgb early stop 
+OPT_ROUNDS = 1000  #To be adjusted based on best validation rounds
+VERBOSE_EVAL = 50 #Print out metric result
+
+df = pd.read_csv('./creditcard.csv')
+
+target = 'Class'
+predictors = ['Time', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10',\
+       'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19',\
+       'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28',\
+       'Amount']
+
+
+train_df, test_df = train_test_split(df, test_size=TEST_SIZE, random_state=RANDOM_STATE, shuffle=True )
+train_df, valid_df = train_test_split(train_df, test_size=VALID_SIZE, random_state=RANDOM_STATE, shuffle=True )
+
+clf = RandomForestClassifier(n_jobs=NO_JOBS, 
+                             random_state=RANDOM_STATE,
+                             criterion=RFC_METRIC,
+                             n_estimators=NUM_ESTIMATORS,
+                             verbose=False)
+
+
+clf.fit(train_df[predictors], train_df[target].values)
+
+preds = clf.predict(valid_df[predictors])
+
+acc = roc_auc_score(valid_df[target].values, preds)
+
+training_score = cross_val_score(clf, train_df[predictors], train_df[target].values, cv=5)
+
+print("Accuracy =",acc)
+print("Train Accuracy =",training_score)
+
+with open("metrics.json", 'w') as outfile:
+        json.dump({ "accuracy": acc, "training_score": training_score[0]}, outfile)
+
+
+# Save the model to a file using pickle
+with open('model.pkl', 'wb') as f:
+    pickle.dump(clf, f)
