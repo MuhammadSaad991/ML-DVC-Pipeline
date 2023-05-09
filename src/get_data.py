db3ca8bdb14cebcd85fc3ce7acf5e4beee45760b
MuhammadSaad991 <i191891@nu.edu.pk>
2023-05-09 22:18:58 +0500

    Add

--- src/get_data.py
+++ src/get_data.py
@@ -13,22 +13,28 @@ if os.path.isfile(file_path):
     print("Already Data available")
     df = pd.read_csv(file_path)
     print(df.head(3))
+    df.to_csv("./data/creditcard.csv")
 else:
-    with dvc.api.open(path=file_path, mode='r', remote=remote, repo='.') as fd:
-        print("Access data gdrive")
-        df = pd.read_csv(fd)
-        print(df.head(3))
+    subprocess.run(['kaggle', 'datasets', 'download', '-d', 'mlg-ulb/creditcardfraud'])
+    with zipfile.ZipFile('./creditcardfraud.zip', 'r') as zip_ref:
+        zip_ref.extractall('./data')
+    os.remove("creditcardfraud.zip")
+    
+    # with dvc.api.open(path=file_path, mode='r', remote=remote, repo='.') as fd:
+    #     print("Access data gdrive")
+    #     df = pd.read_csv(fd)
+    #     print(df.head(3))
 
-        print("data shape gdrive",df.shape)
+    #     print("data shape gdrive",df.shape)
 
-        df.to_csv("./data/creditcard.csv")
+    #     df.to_csv("./data/creditcard.csv")
 
-        if df.empty:
-            subprocess.run(['kaggle', 'datasets', 'download', '-d', 'mlg-ulb/creditcardfraud'])
+        # if df.empty:
+        #     subprocess.run(['kaggle', 'datasets', 'download', '-d', 'mlg-ulb/creditcardfraud'])
             
-            with zipfile.ZipFile('./creditcardfraud.zip', 'r') as zip_ref:
-                zip_ref.extractall('./data')
-            os.remove("creditcardfraud.zip")
+        #     with zipfile.ZipFile('./creditcardfraud.zip', 'r') as zip_ref:
+        #         zip_ref.extractall('./data')
+        #     os.remove("creditcardfraud.zip")
 
 
         
